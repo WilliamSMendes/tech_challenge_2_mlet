35f9bfc5b8baf3e956def41064ebb2e47d4c8d32
william.mendes <william.mendes@acerto.com.br>
2025-12-09 20:34:11 -0300

    v1_extract

--- extract.py
+++ extract.py
@@ -1,1 +1,39 @@
-print("Hello from extract.py")
+from pyspark.sql import SparkSession
+import yfinance as yf
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+def main():
+    cod = "B3SA3.SA"
+    period = "1d"
+
+    spark = SparkSession.builder.getOrCreate()
+
+    logger.info(f"Fetching {cod} data for period: {period}")
+    stock = yf.Ticker(cod)
+    data = stock.history(period)
+
+    spark_df = spark.createDataFrame(data)
+
+    spark_df = spark_df.drop("Dividends", "Stock Splits")
+
+    spark_df = (
+        spark_df
+        .withColumnRenamed("Open", "open")
+        .withColumnRenamed("High", "high")
+        .withColumnRenamed("Low", "low")
+        .withColumnRenamed("Close", "close")
+        .withColumnRenamed("Volume", "volume")
+        .withColumnRenamed("Dividends", "dividends")
+        .withColumnRenamed("Stock Splits", "stock_splits")
+    )
+
+    spark_df.show()
+
+if __name__ == "__main__":
+    main()
